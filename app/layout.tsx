a5d569f1bd910ed2351b1a4c98e2dca8d95b8bac
KCeire <keith@abanteproductions.com>
2025-09-26 17:20:14 +0100

    update bottom nave and added shop to home page cards 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./onchainkit-fix.css";
 import "./theme.css";
 import "@coinbase/onchainkit/styles.css";
 import "./globals.css";
+import { Suspense } from "react";
 import { Providers } from "./providers";
 import { ThemeProvider } from "./components/theme/ThemeProvider";
 import { BottomNav } from "./components/navigation/BottomNav";
@@ -27,7 +28,9 @@ export default function RootLayout({
               {children}
             </div>
             <FloatingCartButton />
-            <BottomNav />
+            <Suspense fallback={<div className="fixed bottom-0 left-0 right-0 h-16 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700"></div>}>
+              <BottomNav />
+            </Suspense>
             <ToastContainer />
           </Providers>
         </ThemeProvider>
